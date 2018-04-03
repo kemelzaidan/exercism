994dde5b385a20867b5314f0c0f7a131bbcf5966
Kemel Zaidan <kemelzaidan@gmail.com>
2018-04-03 17:23:20 -0300

    better solution for isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,34 +1,22 @@
 package isogram
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
-// IsIsogram finds out if a word is a isogram
+// IsIsogram tells you whether passed word is an Isogram or not.
 func IsIsogram(word string) bool {
-	var count = make(map[rune]int)
-
-	// creates a count map with the count of letters in a word
-	for _, letter := range strings.ToLower(word) {
-		_, ok := count[letter]
-		if ok {
-			count[letter]++
-		} else {
-			if letter != '-' && letter != ' ' {
-				count[letter] = 1
+	upperCase := strings.ToUpper(word)
+	m := make(map[rune]bool)
+	for _, letter := range upperCase {
+		if unicode.IsLetter(letter) {
+			if m[letter] == true {
+				return false
 			}
+			m[letter] = true
 		}
 	}
 
-	result := 0
-
-	for _, value := range count {
-		if value > result {
-			result = value
-		}
-	}
-
-	if result > 1 {
-		return false
-	} else {
-		return true
-	}
+	return true
 }
