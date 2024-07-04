1140bc3820c8727d2159243be67f30fdf28b2394
Kemel Zaidan <kemelzaidan@gmail.com>
2024-07-04 15:16:51 -0300

    bird count recursive solution

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,11 +1,10 @@
 defmodule BirdCount do
   def today([]), do: nil
-  def today(list), do: hd(list) 
+  def today([head | _tail]), do: head 
   
   def increment_day_count([]), do: [1]
-  def increment_day_count(list) do
-    [head | tail] = list
-    [head + 1 | tail]
+  def increment_day_count([head | tail]) do
+        [head + 1 | tail]
   end
 
   def has_day_without_birds?(list) do
@@ -13,11 +12,11 @@ defmodule BirdCount do
   end
 
   def total([]), do: 0
-  def total(list) do
-    Enum.reduce(list, &(&1 + &2))
-  end
+  def total([head | tail]), do: head + total(tail)
 
-  def busy_days(list) do
-    Enum.filter(list, &(&1 >= 5)) |> length()
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head >= 5 do
+    1 + busy_days(tail)  
   end
+  def busy_days([_head | tail]), do: busy_days(tail)
 end
