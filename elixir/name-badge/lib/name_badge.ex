e0ab55fa822ea9903142d315c00a650ae0ade10b
Kemel Zaidan <kemelzaidan@gmail.com>
2024-07-14 00:14:27 -0300

    name-badge alternative solution

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,17 +1,16 @@
 defmodule NameBadge do
-  def print(id, name, department) when is_nil(id) and is_nil(department) do
-    "#{name} - OWNER"
-  end
-
-  def print(id, name, department) when id === nil do
-    "#{name} - #{String.upcase(department)}"
-  end
-
-  def print(id, name, department) when department === nil do
-    "[#{id}] - #{name} - OWNER"
-  end
-
   def print(id, name, department) do
-    "[#{id}] - #{name} - #{String.upcase(department)}"
+    dep =
+      if department do
+        String.upcase(department)
+      else
+        "OWNER"
+      end
+
+    if id do
+      "[#{id}] - #{name} - #{dep}"
+    else
+      "#{name} - #{dep}"
+    end
   end
 end
