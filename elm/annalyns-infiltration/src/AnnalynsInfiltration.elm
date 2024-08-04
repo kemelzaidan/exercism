04b0b2a54265390aa6811a4aa09f9137a9655029
Kemel Zaidan <kemelzaidan@gmail.com>
2024-08-03 22:39:03 -0300

    removes elm-stuff

--- elm/annalyns-infiltration/src/AnnalynsInfiltration.elm
+++ elm/annalyns-infiltration/src/AnnalynsInfiltration.elm
@@ -3,44 +3,23 @@ module AnnalynsInfiltration exposing (canFastAttack, canFreePrisoner, canSignalP
 
 canFastAttack : Bool -> Bool
 canFastAttack knightIsAwake =
-    if knightIsAwake then
-        False
-
-    else
-        True
+    not knightIsAwake
 
 
 canSpy : Bool -> Bool -> Bool -> Bool
 canSpy knightIsAwake archerIsAwake prisonerIsAwake =
-    if knightIsAwake || archerIsAwake || prisonerIsAwake then
-        True
-
-    else
-        False
+    knightIsAwake || archerIsAwake || prisonerIsAwake
 
 
 canSignalPrisoner : Bool -> Bool -> Bool
 canSignalPrisoner archerIsAwake prisonerIsAwake =
-    if not archerIsAwake && prisonerIsAwake then
-        True
-
-    else
-        False
+    prisonerIsAwake && not archerIsAwake
 
 
 canFreePrisoner : Bool -> Bool -> Bool -> Bool -> Bool
 canFreePrisoner knightIsAwake archerIsAwake prisonerIsAwake petDogIsPresent =
-    if not knightIsAwake && not archerIsAwake && prisonerIsAwake then
-        True
-
-    else if knightIsAwake && not archerIsAwake && petDogIsPresent then
-        True
-
-    else if not knightIsAwake && not archerIsAwake && petDogIsPresent then
-        True
-
-    else
-        False
+    (petDogIsPresent && not archerIsAwake)
+        || (prisonerIsAwake && not knightIsAwake && not archerIsAwake)
 
 
 stealthAttackDamage : Bool -> Int
